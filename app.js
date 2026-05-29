1f39140154460eb3023cc2feb2521b4a58e606e9
Diarrassouba Khadara <kader@Host-004.lan>
2026-05-29 11:49:58 +0200

    Set quick track card label and search icon to brand orange color

--- app.js
+++ app.js
@@ -475,7 +475,7 @@ function renderHome() {
 
         <!-- Quick Track -->
         <div class="track-card" style="margin-top:18px">
-          <div class="track-card-label" style="color:#6B7280">${icon('search', 13, '#6B7280')} &nbsp;Suivi rapide</div>
+          <div class="track-card-label" style="color:#FF6C00">${icon('search', 13, '#FF6C00')} &nbsp;Suivi rapide</div>
           <div class="track-input-row">
             <input class="track-input" id="home-track-input"
               placeholder="Ex: CD202605290001CI"
